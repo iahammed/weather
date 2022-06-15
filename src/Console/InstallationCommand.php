72d6106de4028cca3b132814c3e1b7455054c35c
M Iftakher Ahammed <iahammed@gmail.com>
2022-06-15 19:53:06 +0100

    Update Console command

--- src/Console/InstallationCommand.php
+++ src/Console/InstallationCommand.php
@@ -6,8 +6,6 @@ use Illuminate\Console\Command;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Input\InputArgument;
 
-
-
 class InstallationCommand extends Command
 {
     /**
